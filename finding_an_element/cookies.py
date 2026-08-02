8433d3d049302925d1002d5b1118f81660bea63f
Eb1n-Babu <ebin.babu.in@icloud.com>
2026-08-02 15:45:02 +0400

    Add explicit and implicit waits to improve Selenium stability

--- finding_an_element/cookies.py
+++ finding_an_element/cookies.py
@@ -11,3 +11,12 @@ x = driver.get_cookies()
 driver.set_window_size(1200,899)
 print(x)
 time.sleep(5)
+
+"""Explicit Wait tells Selenium to wait for a specific condition to 
+happen before moving to the next step.Wait for all elements, globally."""
+
+#driver.implicitly_wait(10)
+#One-line definitions (super short)
+#Explicit Wait: Wait until a specific condition is true.
+
+#Implicit Wait: Wait a fixed time for all element searches.
